4bd2c854a689f7a27e1195c714709a2e84229d43
ms292435 <ms292435@INFO-205-23-10>
2023-11-06 17:28:56 +0100

    Ajout du responsive de toutes les pages sauf savoir-faire

--- WebSite/WebApplication1/wwwroot/js/index.js
+++ WebSite/WebApplication1/wwwroot/js/index.js
@@ -122,6 +122,16 @@ async function fetchInstagramPhotos() {
 }
        
     
+document.addEventListener('DOMContentLoaded', function () {
+    // Sélectionnez le bouton du menu burger et le menu principal
+    const burgerMenu = document.querySelector('.burger-menu');
+    const mainNav = document.querySelector('.mainNav');
 
+    // Ajoutez un écouteur d'événements au bouton du menu burger
+    burgerMenu.addEventListener('click', function () {
+        // Ajoutez ou supprimez la classe 'active' du menu principal
+        mainNav.classList.toggle('active');
+    });
+});
 
 window.onload = main;
