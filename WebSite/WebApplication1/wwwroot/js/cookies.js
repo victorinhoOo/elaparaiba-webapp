5dd144acef5512f07b33cda1632b2a7c3b2623a4
ms292435 <ms292435@INFO-205-23-09>
2023-11-07 18:47:19 +0100

    Ajout de cookie.js

---
+++ WebSite/WebApplication1/wwwroot/js/cookies.js
@@ -0,0 +1,27 @@
+// Fonction pour créer un cookie
+function setCookie(name, value, time) {
+    var expires = "";
+    if (time) {
+        var date = new Date();
+        date.setTime(date.getTime() + (time * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+// Fonction pour obtenir la valeur d'un cookie
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+// Fonction pour supprimer un cookie
+function eraseCookie(name) {
+    document.cookie = name + '=; Max-Age=-99999999;';
+}
