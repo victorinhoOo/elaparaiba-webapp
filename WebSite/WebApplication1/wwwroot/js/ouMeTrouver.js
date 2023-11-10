a54938d5864eecdd2d8fbf0bf29f966bc2a33dc3
Mbigeard06 <mateo.bd@icloud.com>
2023-11-10 11:21:51 +0100

    meTrouver.cs/js/html surMesure.cs/js/html Ajout d'un pop up et d'un try catch pour l'envoi du formulaire

--- WebSite/WebApplication1/wwwroot/js/ouMeTrouver.js
+++ WebSite/WebApplication1/wwwroot/js/ouMeTrouver.js
@@ -25,20 +25,45 @@ window.onload = updatePanierCount();
 document.addEventListener('DOMContentLoaded', function () {
     const form = document.querySelector('form');
     //Création d'un Event pour envoyer le formulaire une fois rempli
-    form.addEventListener('submit', function (event) {
+    form.addEventListener('submit',async function (event) {
         event.preventDefault(); // Empêche l'envoi du formulaire par défaut
 
         const formData = new FormData(form); // Création d'un objet FormData pour le formulaire
 
-        
-
-        fetch('https://localhost:7252/Bijoux/EnvoyerFormulaireOuMeTrouver', {
-            method: 'POST',
-            body: formData // Envoyer le formulaire avec le fichier
-        })
-            .catch(error => {
-                console.error('Erreur:', error);
+        try {
+            const response = await fetch('https://localhost:7252/Bijoux/EnvoyerFormulaireOuMeTrouver', {
+                method: 'POST',
+                body: formData // Envoyer le formulaire avec le fichier
             });
+            if (!response.ok) {
+                throw new Error('Échec de la requête. Statut ' + response.status);
+            }
+            showPopup();
+            console.log('Réponse réussie :', response);
+            // Ajoutez ici le code que vous souhaitez exécuter en cas de succès
+            //Erreur de communication avec le serveur
+        } catch (error) {
+            console.error('Erreur : Communication impossible avec le serveur',);
+        }
+    });
+
+    // Ajout d'un gestionnaire d'événements pour fermer le pop-up
+    const closePopupBtn = document.getElementById('closePopupBtn');
+    closePopupBtn.addEventListener('click', function () {
+        closePopup('.popup');
+        form.reset();
     });
 });
+
+// Fonction pour afficher le pop-up
+function showPopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "block";
+}
+
+// Fonction pour fermer le pop-up
+function closePopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "none";
+}
 
