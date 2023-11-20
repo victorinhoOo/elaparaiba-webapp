1764ff0e76c4ba84a0d5e078a6450fe75cada8fe
Mbigeard06 <mateo.bd@icloud.com>
2023-11-20 22:34:47 +0100

    modificationBijou.js/displayBijouDetails Affichage des attributs d'un bijou.

--- WebSite/WebApplication1/wwwroot/js/modificationBijou.js
+++ WebSite/WebApplication1/wwwroot/js/modificationBijou.js
@@ -20,32 +20,57 @@ async function fetchBijouDetails(bijouId) {
         );
 
         return bijou;
-        
+
     } catch (error) {
         console.error("Erreur de requête:", error);
     }
 }
 
+async function sendBijouModified() {
+    var bijouModified;
+    const form = document.querySelector('form');
+    const formData = new FormData(form);
+    formData.forEach((value, key) => {
+        if (key != "photos[]") {
+            bijouModified.key = value;
+        }
+    });
+}
 
-function displayBijouDetails(bijou){
-    document.getElementById('nom').value = bijou.nomBijou;
+
+//Hydrate les attributs du bijou dans le formulaire
+function displayBijouDetails(bijou) {
+    document.getElementById('name').value = bijou.nomBijou;
     document.getElementById('description').value = bijou.descriptionBijou;
     document.getElementById('quantite').value = bijou.stockBijou;
     document.getElementById('type').value = bijou.type;
-    document.getElementById('matiere').value = bijou.matiere;
+    document.getElementById('matiere').value = "Paladium";
     document.getElementById('prix').value = bijou.prixBijou;
-    document.getElementById('datePublication').value = bijou.datepublication;
+    //Création de la date
+    var partsDate = bijou.datepublication.split('/');
+    // Récupérer les parties de la date
+    var day = partsDate[0];
+    var month = partsDate[1];
+    var year = partsDate[2];
+    // Construire la date au format ISO (aaaa-mm-jj) 
+    var isoDate = year + "-" + month + "-" + day;
+    document.getElementById('datePublication').value = isoDate;
 }
 
 document.addEventListener("DOMContentLoaded", async function () {
-     // Récupérer l'ID du bijou depuis l'URL
-     const queryString = window.location.search;
-     const urlParams = new URLSearchParams(queryString);
-     const bijouId = urlParams.get('bijouId');
+    // Récupérer l'ID du bijou depuis l'URL
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const bijouId = urlParams.get('bijouId');
     //On modifie un bijou existant
-    if(bijouId != -1){
-        const bijou =  await fetchBijouDetails(bijouId);
+    if (bijouId != -1) {
+        var bijou = await fetchBijouDetails(bijouId);
+        console.log(bijou);
         displayBijouDetails(bijou);
     }
-    
+    var form = document.getElementById("bijouForm")
+    form.addEventListener("submit", async function (event) {
+        sendBijouModified();
+    });
+
 });
