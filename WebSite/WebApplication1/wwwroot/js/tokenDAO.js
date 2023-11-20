9c0d90fdbfadb6d90fed9dca4360896e5d52b156
ms292435 <ms292435@INFO-205-23-01>
2023-11-20 18:25:32 +0100

    /js Organisation du JS en séparant le DAO du métier

---
+++ WebSite/WebApplication1/wwwroot/js/tokenDAO.js
@@ -0,0 +1,16 @@
+// Fonction qui permet de créer un nouveau Token
+async function fetchTokenPanier() {
+    var tokenPanier = "";
+    const apiUrl = `https://localhost:7252/Panier/CreerPanierToken`;
+    try {
+        const response = await fetch(apiUrl);
+
+        tokenPanier = await response.text();
+    }
+    catch (error) {
+        console.error("Erreur de requete:", error);
+    }
+    return tokenPanier;
+}	
+
+export { fetchTokenPanier };
