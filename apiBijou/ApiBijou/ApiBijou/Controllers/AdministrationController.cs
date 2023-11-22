faba3fe5520a7c7ab2bcdaad5499d441ea0d8e13
Victor Duboz <dubozvictor@gmail.com>
2023-11-22 10:06:43 +0100

    AdministrationController.cs / Ajout des méthode de connexions administrateur

--- apiBijou/ApiBijou/ApiBijou/Controllers/AdministrationController.cs
+++ apiBijou/ApiBijou/ApiBijou/Controllers/AdministrationController.cs
@@ -1,5 +1,8 @@
-﻿using ApiBijou.Model.Bijoux;
+﻿using API_SAE.Controllers;
+using ApiBijou.Model.Bijoux;
 using ApiBijou.Model.formModel;
+using ApiBijou.Model.Panier;
+using ApiBijou.Model.Utilisateurs;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiBijou.Controllers
@@ -8,6 +11,16 @@ namespace ApiBijou.Controllers
     [Route("Administration")]
     public class AdministrationController : ControllerBase
     {
+        private UtilisateursManager utilisateursManager;
+
+        private PanierManager panierManager;
+
+        public AdministrationController()
+        {
+            this.utilisateursManager = new UtilisateursManager();
+            this.panierManager = new PanierManager();
+        }
+
         /// <summary>
         /// Modifie un bijou
         /// </summary>
@@ -31,5 +44,49 @@ namespace ApiBijou.Controllers
             }
             return result;
         }
+
+        /// <summary>
+        /// Gère la connexion de l'administrateur
+        /// </summary>
+        /// <param name="login">nom d'utilisateur</param>
+        /// <param name="password">mot de passe</param>
+        /// <returns>Ok si la connexion a réussi</returns>
+        [HttpPost("login")]
+        public IActionResult Login(string login, string password)
+        {
+
+            string tokenPanier  = panierManager.CreerPanierToken();
+
+            bool isAdmin = utilisateursManager.ConnectAsAdmin(tokenPanier, login, password);
+            if (isAdmin)
+            {
+                return Ok(new { Message = "Connexion réussie", Token = tokenPanier });
+            }
+            else
+            {
+                return Unauthorized(new { Message = "Échec de l'authentification" });
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="tokenPanier"></param>
+        /// <returns></returns>
+        [HttpGet("isadmin")]
+        public IActionResult IsAdmin(string tokenPanier)
+        {
+            bool isAdmin = utilisateursManager.IsAdmin(tokenPanier);
+            if (isAdmin)
+            {
+                return Ok(new { Message = "L'utilisateur est un administrateur" });
+            }
+            else
+            {
+                return Unauthorized(new { Message = "L'utilisateur n'est pas un administrateur" });
+            }
+        }
+
+
     }
 }
